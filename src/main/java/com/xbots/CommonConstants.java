d890751ada87063dc99bac0a7e6c7883d0de0631
shivigarg <shivi.garg.vce@gmail.com>
2016-10-08 00:14:55 +0530

    creating as backup

---
+++ src/main/java/com/xbots/CommonConstants.java
@@ -0,0 +1,6 @@
+package com.xbots;
+
+public final class CommonConstants {
+public static final String resourceDirLocation = "src/main/resources/";
+public static final String webappDirLocation = "src/main/webapp";
+}
