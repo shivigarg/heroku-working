8dd7b71429a3db2a97f8fa86260e059af3d0240a
shivigarg <shivi.garg.vce@gmail.com>
2016-10-08 16:34:18 +0530

    Update BillingService.java

--- src/main/java/com/xbots/BillingService.java
+++ src/main/java/com/xbots/BillingService.java
@@ -6,6 +6,16 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+
 @Path("/BillingService")
 public class BillingService {
 
@@ -19,4 +29,11 @@ public class BillingService {
       return billingInfoDao.getBillAmount(userid);
    }
    
+   @POST
+	@Path("/tokens")
+	@Produces(MediaType.APPLICATION_XML)
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	public String getTokenValue(@QueryParam("userId") String userId ,@QueryParam("token") String token ,@Context HttpServletResponse servletResponse){
+	      return "200";
+	}
 }
